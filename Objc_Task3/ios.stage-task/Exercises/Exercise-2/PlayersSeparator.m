7f0d99823fbd8df8392b25a17786e8189e610acd
Natalia Lazovskaya <natalia.lazovskaya@innowise.com>
2024-10-23 11:23:01 +0200

    add solution

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,31 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    
+    if (!ratingArray || ratingArray.count < 3) {
+            return 0;
+        }
+    
+    NSInteger count = 0;
+    NSUInteger n = ratingArray.count;
+    
+    for (NSUInteger i = 0; i < n - 2; i++) {
+        for (NSUInteger j = i + 1; j < n - 1; j++) {
+            for (NSUInteger k = j + 1; k < n; k++) {
+
+                if (ratingArray[i].integerValue < ratingArray[j].integerValue &&
+                    ratingArray[j].integerValue < ratingArray[k].integerValue) {
+                    count++;
+                }
+                // Check for decreasing condition
+                else if (ratingArray[i].integerValue > ratingArray[j].integerValue &&
+                         ratingArray[j].integerValue > ratingArray[k].integerValue) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
 }
 
 @end
